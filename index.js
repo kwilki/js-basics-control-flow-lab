76335c7b17919a8bdf66d37d62fd4e4d87498179
Kyle <kylewilkinson14@gmail.com>
2021-10-12 20:44:23 +1100

    started

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceFeet){
+  if(distanceFeet <= 400){
+      return 'This one is on me!'
+  } else if(distanceFeet > 2000){
+      return 'I will gladly take your thirty bucks.'
+  } else if(distanceFeet >= 2500){
+      return 'No can do.'
+  }
 }
 
 function ternaryCheckCity(){
