d9c649264f052c46db04ecdf8a2cd022c20867dd
Kyle <kylewilkinson14@gmail.com>
2021-10-14 10:46:40 +1100

    completed tests

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function scuberGreetingForFeet(distanceFeet){
   if(distanceFeet <= 400){
       return 'This one is on me!'
-  } else if(distanceFeet > 2000){
-      return 'I will gladly take your thirty bucks.'
-  } else if(distanceFeet >= 2500){
-      return 'No can do.'
-  }
+    } else if(distanceFeet >= 2500){
+        return 'No can do.'
+        } else if(distanceFeet > 2000){
+            return 'I will gladly take your thirty bucks.'
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC")
+    return "Ok, sounds good."
+    else 
+      return "No go."
+      
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  if(generosity === "generous")
+    return "Thank you so much."
+    else if(generosity === "not as generous")
+      return "Thank you."
+      else
+        return "Bye."
 }
